e6bb30fc5bd048e3f0683f9373c3e3c63f511e09
KorenhaV <korenha.v@gmail.com>
2020-10-30 14:28:44 +0200

    Creating Item model + migration + controller

---
+++ database/migrations/2020_10_29_140320_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('category_id');
+            $table->text('description');
+            $table->text('consist');
+            $table->text('caring');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
