8da4c4b82efbb754c68572158868769c5ae16ee7
KorenhaV <korenha.v@gmail.com>
2020-10-30 19:33:31 +0200

    Created navbar

---
+++ resources/views/main.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('navbar')
+            <nav class="navbar navbar-expand-lg bg-white">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="container">
+                    <a class="navbar-brand" href="#!">
+                        <img src="{{ URL::asset('images/B8_Logo.svg') }}">
+                    </a>
+
+                    <div class="collapse navbar-collapse" id="navbarToggler">
+                            <ul class="navbar-nav ml-auto text-uppercase">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#!">News</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#!">Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#!">Partners</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#!">About</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#!">Shop</a>
+                                </li>
+                            </ul>
+                    </div>
+                </div>
+            </nav>
+@endsection
