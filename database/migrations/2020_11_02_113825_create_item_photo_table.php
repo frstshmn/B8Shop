76608dc0b29301d8db67b7207b411c6cf1b52d52
KorenhaV <korenha.v@gmail.com>
2020-11-02 14:14:55 +0200

    Rename 'photos' , 'sizes', add 'x_item_sizes'
    Due to task, renamed tables and migrations for them. Also added x_item_sizes table to relink items and sizes tables

--- database/migrations/2020_11_02_113825_create_item_photo_table.php
+++ database/migrations/2020_11_02_113825_create_item_photo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhotosTable extends Migration
+class CreateItemPhotoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePhotosTable extends Migration
      */
     public function up()
     {
-        Schema::create('photos', function (Blueprint $table) {
+        Schema::create('item_photo', function (Blueprint $table) {
             $table->id();
             $table->integer('item_id');
             $table->string('photo_link');
@@ -28,6 +28,6 @@ class CreatePhotosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('photos');
+        Schema::dropIfExists('item_photo');
     }
 }
--- database/migrations/2020_11_02_115418_create_x_items_sizes_table.php
+++ database/migrations/2020_11_02_115418_create_x_items_sizes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSizesTable extends Migration
+class CreateXItemSizesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateSizesTable extends Migration
      */
     public function up()
     {
-        Schema::create('sizes', function (Blueprint $table) {
+        Schema::create('x_items_sizes', function (Blueprint $table) {
             $table->id();
             $table->integer('item_id');
-            $table->string('size');
+            $table->integer('size_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateSizesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sizes');
+        Schema::dropIfExists('x_item_sizes');
     }
 }
