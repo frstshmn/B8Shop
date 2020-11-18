a144aeb7ec97920441874711a012a42c3678f2e1
KorenhaV <korenha.v@gmail.com>
2020-11-18 20:00:21 +0200

    Add javadoc coments

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use App\Models\OrderList ;
+use App\Models\OrderList;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
 
 class OrderController extends Controller
 {
+    /** Render data to show order
+     * @method GET
+     * @param id - identifier of category (received from wildcard)
+     * @return JSON - json encoded item data
+    */
+    public function show($id){
+        $order = Order::where('id', $id)->first();
+        return json_encode($order);
+    }
+
+    /** Create new order
+     * @method POST
+     * @param request - values to create in order table
+     * @return JSON - json encoded item data
+    */
     public function create(Request $request){
         $order = new Order;
         $order->first_name = $request->first_name;
@@ -37,5 +52,37 @@ class OrderController extends Controller
             $order_list->price = $basket_item->price;
             $order_list->save();
         }
+    }
+
+    /** Update existing order
+     * @method PUT
+     * @param request - values to update in order table
+     * @return JSON - json encoded item data
+    */
+    public function update(Request $request){
+        $order = Order::where('id', $request->id)->first();
+        $order->first_name = $request->first_name;
+        $order->last_name = $request->last_name;
+        $order->email = $request->email;
+        $order->phone = $request->phone;
+        $order->country = $request->country;
+        $order->city = $request->city;
+        $order->shipping = $request->shipping;
+        $order->warehouse = $request->warehouse;
+        $order->comments = $request->comments;
+        $order->payment_method = $request->payment_method;
+        $order->status = "Pending";
+
+        $order->save();
+    }
+
+    /** Delete particular order
+     * @method DELETE
+     * @param request - values
+     * @return JSON - json encoded item data
+    */
+    public function delete(Request $request){
+        $order = Order::where('id', $request->id)->first();
+        $order->delete();
     }
 }
